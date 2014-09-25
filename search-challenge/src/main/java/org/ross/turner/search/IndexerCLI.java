e76b205b129072ca1b4e04f20b34b17c0fbb983c
rossturner <ross.john.turner@googlemail.com>
2014-09-26 01:10:56 +0200

    Modified POM and Indexer

--- search-challenge/src/main/java/org/ross/turner/search/IndexerCLI.java
+++ search-challenge/src/main/java/org/ross/turner/search/IndexerCLI.java
@@ -1,6 +1,10 @@
 package org.ross.turner.search;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
 
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
@@ -9,145 +13,245 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.benchmark.byTask.feeds.DocMaker;
+//import org.apache.lucene.benchmark.byTask.utils.FileUtils;
 import org.apache.lucene.benchmark.utils.ExtractWikipedia;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.index.IndexableFieldType;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+import org.apache.commons.io.FileUtils;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *<p>
+ * <p>
  * Command line interface for indexing Wikipedia articles.
  * 
  * usage: org.ross.turner.search.IndexerCLI:
  * <li>
- * <ul>--input <input>        the input wikipedia aritcle file</ul>
- * <ul>--numThreads <numThreads>   the number of threads to use for indexing</ul>
- * <ul>--output <output>      the output path</ul>
+ * <ul>
+ * --input <input> the input wikipedia aritcle file
+ * </ul>
+ * <ul>
+ * --numThreads <numThreads> the number of threads to use for indexing
+ * </ul>
+ * <ul>
+ * --output <output> the output path
+ * </ul>
  * </li>
- *</p>
+ * </p>
  */
-public class IndexerCLI 
-{
-    private static final String INPUTOPTION = "input";
-    
-    private static final String OUTPUTOPTION = "output";
-    
-    private static final String NUMTHREADSOPTION = "numThreads";
-	
-    private static final Logger LOGGER = LoggerFactory.getLogger(IndexerCLI.class);
-    
-    public static void main( String[] args )
-    {
-        
-    	CommandLineParser parser = new BasicParser();
-        
-        // add options          
-        
-        Options options = new Options();
-        
-        Option intOpt = OptionBuilder.withArgName(INPUTOPTION).withLongOpt(INPUTOPTION).hasArg().withDescription("the input wikipedia aritcle file").isRequired(true).create();
+public class IndexerCLI {
+	private static final String INPUTOPTION = "input";
 
-        options.addOption(intOpt);
-        
-        Option sumOpt = OptionBuilder.withArgName(OUTPUTOPTION).withLongOpt(OUTPUTOPTION).hasArg().withDescription("the output path").isRequired(true).create();
-        
-        options.addOption(sumOpt);
-        
-        Option siteOpt = OptionBuilder.withArgName(NUMTHREADSOPTION).withLongOpt(NUMTHREADSOPTION).hasArg().withDescription("the number of threads to use for indexing (default is 1)").isRequired(false).create();
-        
-        options.addOption(siteOpt);
+	private static final String OUTPUTOPTION = "output";
 
-        HelpFormatter formatter = new HelpFormatter();
-        
-        CommandLine line = null;
-        
-        try {
-                
-                line = parser.parse( options, args );
-        
-        } catch (org.apache.commons.cli.ParseException e) {
-        
-        		e.printStackTrace();
-        	
-                System.err.println("Invalid input arguments");
-                
-                formatter.printHelp(IndexerCLI.class.getName(), options);
-                
-                System.exit(2);
-        
-        }
-        
-        File articles = new File(line.getOptionValue(INPUTOPTION));
-        
-        if(!articles.exists()){
-        	
-        	outputFileArgumentErrorAndExit(articles.getAbsolutePath(),formatter,options);
-        	
-        }
-        
-        LOGGER.info("Reading articles from " + articles.getAbsolutePath());
-        
-        File output = new File(line.getOptionValue(OUTPUTOPTION));
-        
-        LOGGER.info("Using output path " + output.getAbsolutePath());
-        
-        int numThreads = 1;
-        
-        try{
-        
-        	numThreads = Integer.valueOf(line.getOptionValue(NUMTHREADSOPTION));
-        
-        }catch(NumberFormatException e){
-        	
-        	outputInvalidNumThreadsErrorAndExit(line.getOptionValue(NUMTHREADSOPTION),formatter,options);
-        	
-        }
-        
-        if(numThreads < 1){
-        	
-        	numThreads = 1;
-        
-        }
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(IndexerCLI.class);
 
-        LOGGER.info("Using " + numThreads + " threads");
-         
-        String extractArgs[] = new String[]{"-i",articles.getAbsolutePath(),"-o",output.getAbsolutePath(),"-d"};
-        
-        try {
-        	
-			ExtractWikipedia.main(extractArgs);
-			
-		} catch (Exception e) {
-	
-			LOGGER.error("Error extracting articles: " + e.getMessage());
-			
+	public static void main(String[] args) {
+
+		CommandLineParser parser = new BasicParser();
+
+		// add options
+
+		Options options = new Options();
+
+		Option intOpt = OptionBuilder.withArgName(INPUTOPTION)
+				.withLongOpt(INPUTOPTION).hasArg()
+				.withDescription("the input wikipedia aritcle file")
+				.isRequired(true).create();
+
+		options.addOption(intOpt);
+
+		Option outOpt = OptionBuilder.withArgName(OUTPUTOPTION)
+				.withLongOpt(OUTPUTOPTION).hasArg()
+				.withDescription("the output path").isRequired(true).create();
+
+		options.addOption(outOpt);
+
+		HelpFormatter formatter = new HelpFormatter();
+
+		CommandLine line = null;
+
+		try {
+
+			line = parser.parse(options, args);
+
+		} catch (org.apache.commons.cli.ParseException e) {
+
+			System.err.println("Invalid input arguments");
+
+			formatter.printHelp(IndexerCLI.class.getName(), options);
+
+			System.exit(2);
+
 		}
 
-        
-    }
-    
-    public IndexerCLI(){
-    	
-    }
-    
-    
-    private static void outputInvalidNumThreadsErrorAndExit(String numThreads, HelpFormatter formatter, Options options){
-        
-        System.err.println("Invalid" + NUMTHREADSOPTION +" argument: " + numThreads + ", argument should be a number >= 1");
-        
-        formatter.printHelp(IndexerCLI.class.getName(), options);
-        
-        System.exit(2);
-        
-    }
-    
-    private static void outputFileArgumentErrorAndExit(String path, HelpFormatter formatter, Options options){
-        
-        System.err.println("Path does not exist: " + path);
-        
-        formatter.printHelp(IndexerCLI.class.getName(), options);
-        
-        System.exit(2);
-    }
-       
+		File articles = new File(line.getOptionValue(INPUTOPTION));
+
+		if (!articles.exists()) {
+
+			outputFileArgumentErrorAndExit(articles.getAbsolutePath(),
+					formatter, options);
+
+		}
+
+		LOGGER.info("Reading articles from " + articles.getAbsolutePath());
+
+		File output = new File(line.getOptionValue(OUTPUTOPTION));
+
+		LOGGER.info("Using output path " + output.getAbsolutePath());
+
+		File dumpOutput = new File(output.getAbsolutePath() + File.separator
+				+ "dump");
+
+		String extractArgs[] = new String[] { "-i", articles.getAbsolutePath(),
+				"-o", dumpOutput.getAbsolutePath(), "-d" };
+
+		try {
+
+			ExtractWikipedia.main(extractArgs);
+
+		} catch (Exception e) {
+
+			LOGGER.error("Error extracting articles: " + e.getMessage());
+
+			System.exit(1);
+
+		}
+
+		FSDirectory fs = null;
+
+		try {
+
+			LOGGER.info("Indexing extracted articles in "
+					+ dumpOutput.getAbsolutePath());
+
+			fs = FSDirectory.open(output);
+
+		} catch (IOException e) {
+
+			LOGGER.error("Error reading extracted articles: " + e.getMessage());
+
+			System.exit(1);
+
+		}
+
+		StandardAnalyzer analyzer = new StandardAnalyzer(Version.LATEST);
+
+		LOGGER.info("Using " + analyzer.getClass().getName()
+				+ " for tokenisation");
+
+		IndexWriterConfig config = new IndexWriterConfig(Version.LATEST,
+				analyzer);
+
+		config.setOpenMode(OpenMode.CREATE);
+
+		config.setRAMBufferSizeMB(256);
+		
+		IndexWriter indexWriter = null;
+
+		try {
+
+			indexWriter = new IndexWriter(fs, config);
+			
+			File dir = new File(dumpOutput.getAbsolutePath());
+
+			Iterator<File> files = FileUtils.iterateFiles(dir, null, true);
+
+			int filesIndexed = 0; 
+			
+			while(files.hasNext()) {
+
+				File file = files.next();
+				
+				if(file.isDirectory()){
+					
+					continue;
+					
+				}
+				
+				Document document = new Document();
+
+				String path = file.getCanonicalPath();
+
+				BufferedReader reader = new BufferedReader(new FileReader(file));
+
+				String nextLine = null;
+
+				int offset = 1;
+
+				Document doc = new Document();
+				
+				doc.add(new TextField("path", file.getAbsolutePath(), Store.YES));
+				
+				while ((nextLine = reader.readLine()) != null) {
+
+					switch (offset) {
+						
+						case 5: doc.add(new TextField("body", nextLine, Store.NO));
+						
+						case 4: break;
+						
+						case 3: doc.add(new TextField("title", nextLine, Store.YES));
+						
+						case 2: break;
+						
+						case 1: break;
+						
+						default: break;
+						
+					}
+					
+					offset++;
+
+				}
+				
+				indexWriter.addDocument(document);
+				
+				filesIndexed++;
+				
+			}
+			
+			LOGGER.info("No. of files to indexex: " + filesIndexed);
+
+			indexWriter.commit();
+			
+			indexWriter.close();
+
+		} catch (IOException e) {
+
+			LOGGER.error("Error writing the index: " + e.getMessage());
+
+			System.exit(1);
+
+		}
+
+		LOGGER.info("Indexing finished");
+
+	}
+
+	private static void outputFileArgumentErrorAndExit(String path,
+			HelpFormatter formatter, Options options) {
+
+		System.err.println("Path does not exist: " + path);
+
+		formatter.printHelp(IndexerCLI.class.getName(), options);
+
+		System.exit(2);
+	}
+
 }
